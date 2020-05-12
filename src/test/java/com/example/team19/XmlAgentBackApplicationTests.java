ebdffe8798a2ce34ff076ef4a5124f9049747550
MarinaBartulov <bartulovmarina1@gmail.com>
2020-05-12 12:15:58 +0200

    Project created.

---
+++ src/test/java/com/example/team19/XmlAgentBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlAgentBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
