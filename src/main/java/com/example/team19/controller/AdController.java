6165069f5bce495764b408b9211e64c39ddaea72
MarinaBartulov <bartulovmarina1@gmail.com>
2020-05-13 22:23:39 +0200

    Agent role added

--- src/main/java/com/example/team19/controller/AdController.java
+++ src/main/java/com/example/team19/controller/AdController.java
@@ -3,6 +3,7 @@ package com.example.team19.controller;
 import com.example.team19.dto.AdDTO;
 import com.example.team19.service.impl.AdServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.cert.CertificateEncodingException;
@@ -17,6 +18,7 @@ public class AdController {
     @Autowired
     private AdServiceImpl adService;
 
+    @PreAuthorize("hasRole('ROLE_AGENT')")
     @GetMapping(value="/ads", produces = "application/json")
     public ArrayList<AdDTO> search()  {
         // sada vraca sve oglase, nije implementirana pretraga
